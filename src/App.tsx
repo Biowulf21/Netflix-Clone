1c4d34b12415fdd6fee23086575132177c010aba
James Jilhaney <notail2111@gmail.com>
2022-11-10 13:25:55 +0800

    Added firebase hosting to project

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,28 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./screens/unauthenticated/landing-page";
+import { initializeApp } from "firebase/app";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // Firebase configuration
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBxBVEYy0JHbgBdNwS4epabIcN2eaLanSI",
+
+    authDomain: "fir-clone-d97d1.firebaseapp.com",
+
+    projectId: "fir-clone-d97d1",
+
+    storageBucket: "fir-clone-d97d1.appspot.com",
+
+    messagingSenderId: "421048475493",
+
+    appId: "1:421048475493:web:539e11b32b8873edaccb8c",
+  };
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
 
   return (
     <>
