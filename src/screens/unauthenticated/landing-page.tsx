d19d60d01a86c4799ddab716d8b4fd9ad75ba670
James Jilhaney <notail2111@gmail.com>
2022-11-10 12:20:07 +0800

    Fixed spacing and rearranged z-index and set position

--- src/screens/unauthenticated/landing-page.tsx
+++ src/screens/unauthenticated/landing-page.tsx
@@ -1,43 +1,47 @@
 import React from "react";
 import "./style.css";
+import bg from "../../assets/netflix-hero-bg.jpg";
 
 const LandingPage = () => {
   return (
     <>
-      <header className="header">
-        <h2>Netflix</h2>
-        <div>
-          <select className="language-select">
-            <option defaultChecked value="English">
-              English
-            </option>
-            <option value="Filipino">Filipino</option>
-          </select>
-          <button className="sign-in-btn">Sign In</button>
-        </div>
-      </header>
-      <section>
-        <div className="hero-widget-div">
-          <div className="hero-widget">
-            <h1 className="hero-title">
-              Unlimited movies, TV shows, and more.
-            </h1>
-            <h3 className="hero-subtitle">Watch anywhere. Cancel anytime.</h3>
-            <h4 className="hero-text">
-              Ready to watch? Enter your email to create or restart your
-              membership.
-            </h4>
-            <div className="email-signup-div">
-              <input
-                onFocus={(event) => (event.target.placeholder = "")}
-                className="hero-input"
-                placeholder="Email Address"
-              ></input>
-              <button className="get-started-btn">Get Started</button>
+      <div className="top-section-wrapper">
+        <img className="top-section-bg" src={bg} alt="" />
+        <header className="header">
+          <h2>Netflix</h2>
+          <div>
+            <select className="language-select">
+              <option defaultChecked value="English">
+                English
+              </option>
+              <option value="Filipino">Filipino</option>
+            </select>
+            <button className="sign-in-btn">Sign In</button>
+          </div>
+        </header>
+        <section>
+          <div className="hero-widget-div">
+            <div className="hero-widget">
+              <h1 className="hero-title">
+                Unlimited movies, TV shows, and more.
+              </h1>
+              <h3 className="hero-subtitle">Watch anywhere. Cancel anytime.</h3>
+              <h4 className="hero-text">
+                Ready to watch? Enter your email to create or restart your
+                membership.
+              </h4>
+              <div className="email-signup-div">
+                <input
+                  onFocus={(event) => (event.target.placeholder = "")}
+                  className="hero-input"
+                  placeholder="Email Address"
+                ></input>
+                <button className="get-started-btn">Get Started</button>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 };
